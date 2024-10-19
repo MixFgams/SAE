895483f91461ac2f41ed59ff8c5c6962105d6a7c
MixFgams <b.fmohamedse@gmail.com>
2024-10-19 10:49:04 +0200

    Initialisation

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="icon" src="img/obLogo.png" type="image/x-icon">
+        <link rel="stylesheet" href="style.css">
+        <script src="script.js"></script>
+    </head>
+    <body>
+        <?php include 'otherPages/header.php'?>
+        
+        <?php include 'otherPages/footer.php'?>
+    </body>
+</html>
