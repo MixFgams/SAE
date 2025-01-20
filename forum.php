ae47c3980513c1fa0b9c7f29af7c8a75310a9222
billel hakkas <billel.hakkas@etu.univ-lyon1.fr>
2025-01-20 16:27:44 +0100

    forum css fait

--- forum.php
+++ forum.php
@@ -132,6 +132,8 @@ try {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Messages du Forum</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="forum.css">
+
 </head>
 <body>
 
