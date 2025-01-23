8e061cead696812b9c7793e6ae9aa71548b81b3b
MixFgams <b.fmohamedse@gmail.com>
2025-01-23 23:36:02 +0100

    FEAT : system de refus et de redirection à la page d'accueil si jamais l'utilisateur n'est pas un admin

--- admin/refus.php
+++ admin/refus.php
@@ -3,7 +3,6 @@
 //parametres de profondeur
 $_SESSION["profendeurDossier"] = 1;
 
-
 ?>
 
 <!DOCTYPE html>
@@ -12,6 +11,7 @@ $_SESSION["profendeurDossier"] = 1;
     <meta charset="UTF-8">
     <link rel="stylesheet" href="../style.css"> 
     <title>Refus d'accès</title>
+    <meta http-equiv="refresh" content="5;url=../index.php">
 </head>
 <body>
     <?php include '../pagesOutils/header.php'?>
@@ -20,6 +20,7 @@ $_SESSION["profendeurDossier"] = 1;
         <section>
             <h1>Accès refusé</h1>
             <p>Vous n'avez pas les autorisations nécessaires pour accéder à cette page.</p>
+            <p>Vous serez redirigé vers la page d'accueil dans 5 secondes...</p>
         </section>
     </main>
 
